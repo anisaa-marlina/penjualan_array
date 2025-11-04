e3f46ec61d7338fd2986c2b78fe3b79b807c44eb
anisaa-marlina <anisaamarlinaa@gmail.com>
2025-11-04 16:26:41 +0700

    Menambahkan folder img perhitungan total dan grand total

--- penjualan.php
+++ penjualan.php
@@ -6,8 +6,6 @@ $harga_barang = [7000000, 1500000, 1200000, 150000, 250000];
 echo "<h2>-- POLGAN MART --</h2><hr>";
 
 // Commit 2 – Memilih Barang & Jumlah Acak
-
-// Menentukan berapa variasi barang yang dibeli (acak 1–5)
 $jumlah_variasi = rand(1, 5);
 echo "Jumlah variasi barang dibeli: <b>$jumlah_variasi</b><br><br>";
 
@@ -15,13 +13,9 @@ echo "Jumlah variasi barang dibeli: <b>$jumlah_variasi</b><br><br>";
 $barang_dibeli = [];
 
 for ($i = 0; $i < $jumlah_variasi; $i++) {
-    // Pilih index acak dari array barang
     $index = rand(0, count($nama_barang) - 1);
-
-    // Jumlah barang dibeli (acak 1–5)
     $jumlah_beli = rand(1, 5);
 
-    // Simpan ke array pembelian
     $barang_dibeli[] = [
         "nama" => $nama_barang[$index],
         "harga" => $harga_barang[$index],
@@ -29,19 +23,42 @@ for ($i = 0; $i < $jumlah_variasi; $i++) {
     ];
 }
 
-// Tampilkan hasil pembelian acak
+// Commit 3 – Tambahkan total per barang & grand total
 echo "<table border='1' cellpadding='6' cellspacing='0'>";
-echo "<tr><th>No</th><th>Nama Barang</th><th>Harga</th><th>Jumlah</th></tr>";
+echo "<tr>
+        <th>No</th>
+        <th>Nama Barang</th>
+        <th>Harga</th>
+        <th>Jumlah</th>
+        <th>Total</th>
+      </tr>";
 
 $no = 1;
+$grand_total = 0;
+
 foreach ($barang_dibeli as $item) {
+    // Hitung total per barang
+    $total = $item['harga'] * $item['jumlah'];
+
+    // Tambahkan ke grand total
+    $grand_total += $total;
+
     echo "<tr>";
     echo "<td>$no</td>";
     echo "<td>{$item['nama']}</td>";
     echo "<td>Rp " . number_format($item['harga'], 0, ',', '.') . "</td>";
     echo "<td>{$item['jumlah']}</td>";
+    echo "<td>Rp " . number_format($total, 0, ',', '.') . "</td>";
     echo "</tr>";
+
     $no++;
 }
+
+// Tampilkan Grand Total
+echo "<tr style='font-weight:bold; background-color:#f2f2f2;'>
+        <td colspan='4' align='right'>Grand Total</td>
+        <td>Rp " . number_format($grand_total, 0, ',', '.') . "</td>
+      </tr>";
+
 echo "</table>";
 ?>
