5c6ee7fd4c0af1f4c8f64df1f565273eee6365d2
anisaa-marlina <anisaamarlinaa@gmail.com>
2025-11-04 15:55:01 +0700

    Menambahkan folder img dan setup awal (array nama & header POLGAN MART)

---
+++ penjualan.php
@@ -0,0 +1,7 @@
+<?php
+// Commit 1 – Setup awal
+$nama_barang = ["Laptop", "Printer", "Scanner", "Mouse", "Keyboard"];
+$harga_barang = [7000000, 1500000, 1200000, 150000, 250000];
+
+echo "<h2>-- POLGAN MART --</h2><hr>";
+?>
