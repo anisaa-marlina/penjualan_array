03a16e4cdb288fd2449eec314eb78a30e9175084
anisaa-marlina <anisaamarlinaa@gmail.com>
2025-11-04 16:46:02 +0700

    Revisi hapus kolom kode & ganti gambar output_akhir

--- penjualan.php
+++ penjualan.php
@@ -1,20 +1,21 @@
 <?php
 // Commit 1 – Setup awal
-$nama_barang = ["Laptop", "Printer", "Scanner", "Mouse", "Keyboard"];
-$harga_barang = [7000000, 1500000, 1200000, 150000, 250000];
+$kode_barang = ["B001", "B002", "B003", "B004", "B005"];
+$nama_barang = ["Pensil", "Buku Tulis", "Penghapus", "Pulpen", "Spidol"];
+$harga_barang = [2000, 5000, 1500, 3000, 4500];
 
-echo "<h2>-- POLGAN MART --</h2><hr>";
+echo "<h2 style='text-align:center;'>-- POLGAN MART --</h2>";
 
 // Commit 2 – Memilih Barang & Jumlah Acak
-$jumlah_variasi = rand(1, 5);
-echo "Jumlah variasi barang dibeli: <b>$jumlah_variasi</b><br><br>";
+$jumlah_variasi = rand(3, 5);
+echo "<b>Daftar Barang yang Dibeli:</b><br><br>";
 
 // Array untuk menampung data pembelian
 $barang_dibeli = [];
 
 for ($i = 0; $i < $jumlah_variasi; $i++) {
-    $index = rand(0, count($nama_barang) - 1);
-    $jumlah_beli = rand(1, 5);
+    $index = $i; // ambil urutan sesuai indeks agar tidak duplikat
+    $jumlah_beli = rand(1, 10);
 
     $barang_dibeli[] = [
         "nama" => $nama_barang[$index],
@@ -23,42 +24,39 @@ for ($i = 0; $i < $jumlah_variasi; $i++) {
     ];
 }
 
-// Commit 3 – Tambahkan total per barang & grand total
-echo "<table border='1' cellpadding='6' cellspacing='0'>";
-echo "<tr>
-        <th>No</th>
-        <th>Nama Barang</th>
-        <th>Harga</th>
-        <th>Jumlah</th>
-        <th>Total</th>
+// Commit 4 – Rapikan tampilan tabel dan tampilkan total belanja
+echo "<table border='1' cellpadding='8' cellspacing='0' width='80%' align='center' style='border-collapse:collapse;'>";
+echo "<tr style='background-color:#f2f2f2; text-align:center; font-weight:bold;'>
+        <td>Nama Barang</td>
+        <td>Harga (Rp)</td>
+        <td>Jumlah Beli</td>
+        <td>Total (Rp)</td>
       </tr>";
 
-$no = 1;
 $grand_total = 0;
-
 foreach ($barang_dibeli as $item) {
-    // Hitung total per barang
     $total = $item['harga'] * $item['jumlah'];
-
-    // Tambahkan ke grand total
     $grand_total += $total;
 
-    echo "<tr>";
-    echo "<td>$no</td>";
-    echo "<td>{$item['nama']}</td>";
-    echo "<td>Rp " . number_format($item['harga'], 0, ',', '.') . "</td>";
-    echo "<td>{$item['jumlah']}</td>";
-    echo "<td>Rp " . number_format($total, 0, ',', '.') . "</td>";
-    echo "</tr>";
-
-    $no++;
+    echo "<tr align='center'>
+            <td align='left'>{$item['nama']}</td>
+            <td>" . number_format($item['harga'], 0, ',', '.') . "</td>
+            <td>{$item['jumlah']}</td>
+            <td>" . number_format($total, 0, ',', '.') . "</td>
+          </tr>";
 }
 
-// Tampilkan Grand Total
-echo "<tr style='font-weight:bold; background-color:#f2f2f2;'>
-        <td colspan='4' align='right'>Grand Total</td>
+// Baris grand total
+echo "<tr style='font-weight:bold; background-color:#f9f9f9;'>
+        <td colspan='3' align='right'>Grand Total</td>
         <td>Rp " . number_format($grand_total, 0, ',', '.') . "</td>
       </tr>";
-
 echo "</table>";
+
+// Garis pemisah dan total belanja
+echo "<hr width='80%'>";
+echo "<p align='center'><b>Total Belanja Anda:</b> Rp " . number_format($grand_total, 0, ',', '.') . "</p>";
+
+// Pesan penutup
+echo "<p align='center'><i>Terima kasih sudah berbelanja di POLGAN MART 💕</i></p>";
 ?>
